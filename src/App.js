b408a5c8dc756dfaeb900213c4c1cf32244bcb17
raul <raul@push-founders.com>
2019-04-11 02:05:54 +0200

    Initial state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,189 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        users: [],
+        email: '',
+        password: '',
+        name: '',
+        singleName: '',
+        singleUser: undefined
+    };
+
+
+    getUsers() {
+        const requestBody = {
+            query: `
+                query {
+                    users {
+                        _id
+                        name
+                        email                    
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(({data}) => {
+                console.log({data});
+                if(data) {
+                    this.setState({users: data.users});
+                }
+            })
+            .catch(err => console.log('getUsers', err));
+    }
+
+    getSingleUser() {
+        const requestBody = {
+            query: `
+                query {
+                    user(name: "${this.state.singleName}") {
+                        name
+                        email
+                        _id
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(({data}) => {
+                console.log({data});
+                if(data) {
+                    this.setState({singleUser: data.user});
+                }
+            })
+            .catch(err => console.log('getUsers', err));
+    }
+
+    createUser() {
+        if (this.state.name === '' || !this.state.name) {
+            return
+        }
+        const requestBody = {
+            query: `
+                mutation {
+                    createUser(userInput: {
+                        email: "${this.state.email}"
+                        password: "${this.state.password}"
+                        name: "${this.state.name}"
+                    }) {
+                        _id
+                        email
+                        name
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(({data}) => {
+            if (data) {
+                const users = this.state.users;
+                users.push(data.createUser);
+                this.setState(users)
+            }
+        })
+        .catch(err => console.log('createUser', err));
+    }
+
+
+    onChange(e) {
+        this.setState({[e.target.id]: e.target.value})
+    }
+
+    render() {
+        return (
+            <div style={styles.container} className="App">
+
+                <button type={'button'} onClick={this.createUser.bind(this)} style={styles.button}>Create User</button>
+                <div style={styles.form}>
+                    <div style={styles.formItem} className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" id="email" onChange={this.onChange.bind(this)}/>
+                    </div>
+                    <div style={styles.formItem} className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="text" id="password" onChange={this.onChange.bind(this)} />
+                    </div>
+                    <div style={styles.formItem} className="form-control">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" onChange={this.onChange.bind(this)} />
+                    </div>
+                </div>
+                <button type={'button'} onClick={this.getSingleUser.bind(this)} style={styles.button2}>Get User</button>
+                <div style={styles.form}>
+                    <div style={styles.formItem} className="form-control">
+                        <label htmlFor="singleName">Search User</label>
+                        <input type="text" id="singleName" onChange={this.onChange.bind(this)} />
+                    </div>
+                </div>
+                {this.state.singleUser && (
+                    <div style={styles.singleItem}>
+                        {this.state.singleUser._id && (<div style={styles.itemContent}>
+                            <p>Id:</p>
+                            <p>{this.state.singleUser._id}</p>
+                        </div>)}
+                        <div style={{...styles.itemContent, ...styles.border}}>
+                            <p>Name:</p>
+                            <p>{this.state.singleUser.name}</p>
+                        </div>
+                        <div style={{...styles.itemContent, ...styles.border}}>
+                            <p>Email:</p>
+                            <p>{this.state.singleUser.email}</p>
+                        </div>
+                    </div>
+                )}
+
+                <button type={'button'} onClick={this.getUsers.bind(this)} style={styles.button2}>Get Users</button>
+                <ul style={styles.list}>
+                    {this.state.users.length > 0 && (
+                        <h1 style={styles.title}>Users</h1>
+                    )}
+                    {this.state.users.map((user, index) => (
+                        <li key={index} style={styles.listItem}>
+                            {user._id && (<div style={styles.itemContent}>
+                                <p>Id:</p>
+                                <p>{user._id}</p>
+                            </div>)}
+                            <div style={{...styles.itemContent, ...styles.border}}>
+                                <p>Name:</p>
+                                <p>{user.name}</p>
+                            </div>
+                            <div style={{...styles.itemContent, ...styles.border}}>
+                                <p>Email:</p>
+                                <p>{user.email}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,79 @@
+export default {
+    container: {
+        width: '50%',
+        margin: '10px auto',
+        fontFamily: 'sans-serif',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    form: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        marginBottom: 30,
+        minWidth: 300,
+        maxWidth: 600
+    },
+    button: {
+        marginBottom: 10,
+        width: 200,
+        height: 50,
+        backgroundColor: 'darkblue',
+        fontSize: 18,
+        color: 'white',
+        borderRadius: 5
+    },
+    formItem: {
+        flex: 1,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        marginBottom: 10
+
+    },
+    button2: {
+        marginBottom: 10,
+        width: 200,
+        height: 50,
+        backgroundColor: 'darkolivegreen',
+        fontSize: 18,
+        color: 'white',
+        borderRadius: 5
+    },
+    listItem: {
+        display: 'flex',
+        borderBottom: '1px solid lightgrey',
+        width: '100%'
+    },
+    singleItem: {
+        display: 'flex',
+        borderTop: '1px solid lightgrey',
+        borderBottom: '1px solid lightgrey',
+        width: '100%',
+        marginBottom: 50
+    },
+    itemContent: {
+        flex: 1,
+        textAlign: 'center',
+        margin: '0 5px'
+    },
+    list: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        margin: 0,
+        padding: 0
+    },
+    title: {
+        borderBottom: '1px solid lightgrey',
+        width: '100%',
+        textAlign: 'center',
+        margin: 0,
+        padding: 0
+    },
+    border: {
+        borderLeft: '1px solid lightgrey'
+    }
+}
